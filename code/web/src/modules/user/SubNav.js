2750715ddfd9e8abdad5f1b9582ad2a6c360edd8
gaj23 <gabriellejoyce1930@gmail.com>
2021-03-27 17:17:45 -0600

    Start logic for active state of subnav buttons

--- code/web/src/modules/user/SubNav.js
+++ code/web/src/modules/user/SubNav.js
@@ -9,30 +9,58 @@ import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import Button from '../../ui/button'
 import { grey, grey2, black } from '../../ui/common/colors'
+// import { primary, secondary } from '../..ui/common/gradients'
 
 //App Imports
 import userRoutes from '../../setup/routes/user'
 import { logout } from './api/actions'
 
-const SubNav = (props) => (
-  <Grid style={{ backgroundColor: grey }}>
-    <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+const SubNav = (props) => {
+  {/*
+    I'm trying to set up for setting the theme as either 'primary' (purple) or 'secondary' (orange) depending on which subtab is open.
+
+    I think I'm close, but not quite.
+
+    const assessTheme = () => {
+      if (window.location.pathname ===    userRoutes.profile.path) {
+        return secondary
+      } else if (window.location.pathname === userRoutes.profile.path) {
+        return secondary
+      }
+    }
+  */}
+
+
+    return (
+      <Grid style={{ backgroundColor: grey }}>
+        <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+
         <Link to={userRoutes.profile.path}>
-          <Button theme="primary" style={{ marginLeft: '1em' }}>My Account</Button>
+          <Button theme='primary' style={{ marginLeft: '1em' }}>My Account</Button>
         </Link>
 
         <Link to={userRoutes.products.path}>
-          <Button theme="primary" style={{ marginLeft: '1em' }}>My Products</Button>
+          <Button theme='primary' style={{ marginLeft: '1em' }}>My Products</Button>
         </Link>
 
         <Button onClick={props.logout} theme="primary" style={{ marginLeft: '1em' }}>Logout</Button>
-    </GridCell>
-  </Grid>
-)
+        </GridCell>
+      </Grid>
+  )
+}
 
 // Component Properties
 SubNav.propTypes = {
   logout: PropTypes.func.isRequired
 }
 
-export default SubNav
+//do we need a defaultProps like was used in MenuItem to manipulate active state?
+
+// Component State
+function subNavState(state) {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(subNavState, { logout })(SubNav)
