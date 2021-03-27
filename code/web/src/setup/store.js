f4b917bcc324b045a9cfc5fb46ba04fd33139732
gaj23 <gabriellejoyce1930@gmail.com>
2021-03-27 12:57:55 -0600

    Add chrome tools to store

--- code/web/src/setup/store.js
+++ code/web/src/setup/store.js
@@ -1,6 +1,8 @@
 // Imports
 import { compose, combineReducers } from 'redux'
 import { createStore, applyMiddleware } from 'redux'
+//This will allow chrome dev tools access to the store to display in the console.
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
 // App Imports
@@ -40,7 +42,7 @@ export const store = createStore(
   rootReducer,
   initialState,
 
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
   )
 )
