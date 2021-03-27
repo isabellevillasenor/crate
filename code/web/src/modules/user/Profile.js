1e3657887c2626601e2d15b95e2b470c8e0ca4fa
tashiad <tashiadavis10@gmail.com>
2021-03-27 14:27:14 -0600

    Add Logout and My Products buttons to subnav

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -27,36 +27,23 @@ const Profile = (props) => (
 
     <Grid style={{ backgroundColor: grey }}>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <Menu>
-          <MenuItem to={userRoutes.profile.path} type="primary" style={{ color: black }}>My Account</MenuItem>
+          <Link to={userRoutes.profile.path}>
+            <Button theme="primary" style={{ marginLeft: '1em' }}>My Account</Button>
+          </Link>
 
-          {/*<MenuItem section="products" type="primary" style={{ color: black }}>My Products</MenuItem>*/}
+          <Link to={userRoutes.products.path}>
+            <Button theme="primary" style={{ marginLeft: '1em' }}>My Products</Button>
+          </Link>
 
-          {/*<MenuItem type="primary" style={{ color: black }}>Logout</MenuItem>*/}
-        </Menu>
+          <Button onClick={props.logout} theme="primary" style={{ marginLeft: '1em' }}>Logout</Button>
       </GridCell>
     </Grid>
-
-    <Button theme="secondary" style={{ marginLeft: '1em' }}>My Account</Button>
-    <Button theme="secondary" style={{ marginLeft: '1em' }}>My Products</Button>
-    <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
-
-    {/* Top title bar */}
-    {/*<Grid style={{ backgroundColor: grey }}>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H3 font="secondary">My profile</H3>
-      </GridCell>
-    </Grid>*/}
 
     <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
         <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
 
         {/*<p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>*/}
-
-        {/*<Link to={userRoutes.subscriptions.path}>
-          <Button theme="primary">Subscriptions</Button>
-        </Link>*/}
 
       </GridCell>
     </Grid>
