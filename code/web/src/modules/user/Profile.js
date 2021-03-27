d48092b657ccbfe06dfe02f5f896db2cdd6e0a10
gaj23 <gabriellejoyce1930@gmail.com>
2021-03-27 15:59:30 -0600

    Add form component for updating user state

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -1,14 +1,12 @@
 // Imports
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import { Helmet } from 'react-helmet'
 
 // App Imports
 import MyAccount from './MyAccount'
 
 // Component
-const Profile = (props) => (
+const Profile = () => (
   <div>
     {/* SEO */}
     <Helmet>
@@ -18,19 +16,4 @@ const Profile = (props) => (
   </div>
 )
 
-//Do we need component properties, props, or connect any more?
-
-// Component Properties
-Profile.propTypes = {
-  user: PropTypes.object.isRequired,
-  logout: PropTypes.func.isRequired
-}
-
-// Component State
-function profileState(state) {
-  return {
-    user: state.user
-  }
-}
-
-export default connect(profileState, { logout })(Profile)
+export default Profile
