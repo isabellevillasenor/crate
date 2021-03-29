b319700ce6457f0e4cfb5d74d6665baa38fade2d
tashiad <tashiadavis10@gmail.com>
2021-03-29 12:57:07 -0600

    Clean up code comments

--- code/web/src/modules/user/SubNav.js
+++ code/web/src/modules/user/SubNav.js
@@ -8,8 +8,7 @@ import { Link } from 'react-router-dom'
 import { Grid, GridCell } from '../../ui/grid'
 import { H3, H4 } from '../../ui/typography'
 import Button from '../../ui/button'
-import { grey, grey2, black } from '../../ui/common/colors'
-// import { primary, secondary } from '../..ui/common/gradients'
+import { grey } from '../../ui/common/colors'
 
 //App Imports
 import userRoutes from '../../setup/routes/user'
@@ -18,41 +17,26 @@ import { logout } from './api/actions'
 const SubNav = (props) => {
   const currentPage = window.location.pathname
   const checkColor = linkName => {
-    return currentPage === linkName 
+    return currentPage === linkName
       ? 'secondary'
       : 'primary'
   }
-  {/*
-    
-    I'm trying to set up for setting the theme as either 'primary' (purple) or 'secondary' (orange) depending on which subtab is open.
 
-    I think I'm close, but not quite.
+  return (
+    <Grid style={{ backgroundColor: grey }}>
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
 
-    const assessTheme = () => {
-      if (window.location.pathname ===    userRoutes.profile.path) {
-        return secondary
-      } else if (window.location.pathname === userRoutes.profile.path) {
-        return secondary
-      }
-    }
-  */}
+      <Link to={userRoutes.profile.path}>
+        <Button theme={checkColor('/user/profile')} style={{ marginLeft: '1em' }}>My Account</Button>
+      </Link>
 
+      <Link to={userRoutes.products.path}>
+        <Button theme={checkColor('/user/my-products')} style={{ marginLeft: '1em' }}>My Products</Button>
+      </Link>
 
-    return (
-      <Grid style={{ backgroundColor: grey }}>
-        <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-
-        <Link to={userRoutes.profile.path}>
-          <Button theme={checkColor('/user/profile')} style={{ marginLeft: '1em' }}>My Account</Button>
-        </Link>
-
-        <Link to={userRoutes.products.path}>
-          <Button theme={checkColor('/user/my-products')} style={{ marginLeft: '1em' }}>My Products</Button>
-        </Link>
-
-        <Button onClick={props.logout} theme="primary" style={{ marginLeft: '1em' }}>Logout</Button>
-        </GridCell>
-      </Grid>
+      <Button onClick={props.logout} theme="primary" style={{ marginLeft: '1em' }}>Logout</Button>
+      </GridCell>
+    </Grid>
   )
 }
 
@@ -60,8 +44,6 @@ const SubNav = (props) => {
 SubNav.propTypes = {
   logout: PropTypes.func.isRequired
 }
-
-//do we need a defaultProps like was used in MenuItem to manipulate active state?
 
 // Component State
 function subNavState(state) {
