742738098c77eba9214cacbc535b5d1224093daa
rachelbuchta <rachelbuchta@yahoo.com>
2021-03-29 14:29:22 -0600

    Remove grid portion in profile

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -12,31 +12,7 @@ const Profile = () => (
     <Helmet>
       <title>My Profile - Crate</title>
     </Helmet>
-    <Grid style={{ backgroundColor: grey }}>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-          <Link to={userRoutes.profile.path}>
-            <Button theme="primary" style={{ marginLeft: '1em' }}>My Account</Button>
-          </Link>
-
-          <Link to={userRoutes.products.path}>
-            <Button theme="primary" style={{ marginLeft: '1em' }}>My Products</Button>
-          </Link>
-
-          <Button onClick={props.logout} theme="primary" style={{ marginLeft: '1em' }}>Logout</Button>
-      </GridCell>
-    </Grid>
-
-    <Grid>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
-
-        {/*<p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>*/}
-
-      </GridCell>
-    </Grid>
-
       <MyAccount />
-
   </div>
 )
 
