a3b49c05da34cc75f3479d9c373265c3890b35fb
tashiad <tashiadavis10@gmail.com>
2021-03-29 20:29:26 -0600

    Create email input edit functionality

--- code/web/src/modules/user/MyAccount.js
+++ code/web/src/modules/user/MyAccount.js
@@ -22,118 +22,193 @@ class MyAccount extends PureComponent {
     super(props)
 
     this.state = {
-      
+      image: '',
+      description: 'Fashion lover!',
+      email: 'user@crate.com',
+      address: '7575 E 57th Dr, Denver, CO, 12543',
+      descriptionEditMode: false,
+      emailEditMode: false,
+      addressEditMode: false
     }
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  changeDescriptionEditMode = () => {
+    this.setState({
+      descriptionEditMode: true
+    })
+  }
+
+  changeEmailEditMode = (event) => {
+    event.preventDefault()
+    
+    this.setState({
+      emailEditMode: !this.state.emailEditMode
+    })
+  }
+
+  renderEmailEditView = () => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Input
+          type='text'
+          value=''
+          name='email'
+          placeholder=''
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <Button
+          type="submit"
+          theme="secondary"
+          style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
+          onClick={this.changeEmailEditMode}
+        >
+          ✔️
+        </Button>
+      </div>
+    )
+  }
+
+  renderEmailDefaultView = () => {
+    return (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <p style={{ color: grey2 }}>{this.state.email}</p>
+        <Button
+          type="submit"
+          theme="secondary"
+          style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
+          onClick={this.changeEmailEditMode}
+        >
+          <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
+        </Button>
+      </div>
+    )
+  }
+
+  changeAddressEditMode = () => {
+    this.setState({
+      addressEditMode: true
+    })
+  }
+
   render() {
-    <>
-      <SubNav />
+    return(
+      <>
+        <SubNav />
 
-      <Grid>
-        <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-          <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
+        <Grid>
+          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+            <H4 style={{ marginBottom: '0.5em' }}>User's Name</H4>
 
-          <form style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <form style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 
-            <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '40%', height: '60vh' }}>
+              <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '40%', height: '60vh' }}>
 
-              {/* Profile Image */}
-              <div style={{ width: '200px', height: '200px', position: 'relative', overflow: 'hidden', borderRadius: '50%' }}>
-                <img
-                  src='https://hips.hearstapps.com/countryliving.cdnds.net/17/47/2048x1365/gallery-1511194376-cavachon-puppy-christmas.jpg?resize=768:*'
-                  alt='profile image'
-                  style={{ display: 'inline', margin: '0 auto', marginLeft: '-25%', height: '100%', width: 'auto' }}
+                {/* Profile Image */}
+                <div style={{ width: '200px', height: '200px', position: 'relative', overflow: 'hidden', borderRadius: '50%' }}>
+                  <img
+                    src='https://hips.hearstapps.com/countryliving.cdnds.net/17/47/2048x1365/gallery-1511194376-cavachon-puppy-christmas.jpg?resize=768:*'
+                    alt='profile image'
+                    style={{ display: 'inline', margin: '0 auto', marginLeft: '-25%', height: '100%', width: 'auto' }}
+                  />
+                </div>
+                <File
+                  type='file'
+                  name='image'
+                  text='Upload Image'
+                  value={this.state.image}
+                  onChange={this.handleChange}
                 />
-              </div>
-              <File type='file' name='image' text='Upload Image' />
 
-              {/* Personal Description */}
-              <p style={{ color: grey2 }}><strong>Personal Description:</strong></p>
-              <p style={{ color: grey2 }}>Pickled hops explosion faux bacon food carts Vera Katz Ruby Jewel animal welfare west hills money tall boys stumptown dreadlocks cashew cheese Impossible Burger smoked ham ice cream a dog gym. No, You Go stumptown Portlandia plaid evergreens IPA co-op it's raining again bubble tea Beaverton late-night happy hours neckbeard nothin' muffin pearl district First Thursdays Rocky Butte. Flannel bicycle rights sustainable Santacon mustachioed bartenders beardfest 82nd Avenue of Roses gentrification clouds Snowpacolypse frozen fog dog parks scenester Silicon Forest. Kristian Foden-Vencil fifth quadrant microbrew vegan Gresham Alberta Hawthorne breweries wood-fired oven punk rock Hood to Coast veggie burger DIY Alberta Arts nanobrewery.</p>
-              {/*<Textarea type='textarea' value='' name='description' placeholder='' /> HAVE THIS APPEAR ON CLICK*/}
-              <Button
-                type="submit"
-                theme="secondary"
-                style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
-              >
-                <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
-              </Button>
+                {/* Personal Description */}
+                <p style={{ color: grey2 }}><strong>Personal Description:</strong></p>
+                <p style={{ color: grey2 }}>{this.state.description}</p>
+                <Textarea
+                  type='textarea'
+                  value=''
+                  name='description'
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+                <Button
+                  type="submit"
+                  theme="secondary"
+                  style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
+                  onClick={this.changeDescriptionEditMode}
+                >
+                  <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
+                </Button>
 
-            </section>
+              </section>
 
-            <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '40%', height: '60vh' }}>
+              <section style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', width: '40%', height: '60vh' }}>
 
-              {/* Email */}
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p style={{ color: grey2, marginBottom: '1.5em' }}>Email:</p>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Input
-                    type='text'
-                    value=''
-                    name='email'
-                    placeholder={props.user.details.email}
-                    style={{ textAlign: 'center' }}
-                  />
-                  <Button
-                    type="submit"
-                    theme="secondary"
-                    style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
-                  >
-                    <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
-                  </Button>
+                {/* Email */}
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <p style={{ color: grey2, marginBottom: '1.5em' }}>Email:</p>
+                  {this.state.emailEditMode ? this.renderEmailEditView() : this.renderEmailDefaultView()}
                 </div>
-              </div>
 
-              {/* Mailing Address */}
-              <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <p style={{ color: grey2, marginBottom: '1.5em' }}>Address:</p>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Input
-                    type='text'
-                    value=''
-                    name='address'
-                    placeholder='1675 E 45th Dr, Denver, CO'
-                  />
-                  <Button
-                    type="submit"
-                    theme="secondary"
-                    style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
-                  >
-                    <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
-                  </Button>
+                {/* Mailing Address */}
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                  <p style={{ color: grey2, marginBottom: '1.5em' }}>Address:</p>
+                  <p style={{ color: grey2 }}>{this.state.address}</p>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Input
+                      type='text'
+                      value=''
+                      name='address'
+                      placeholder=''
+                      value={this.state.address}
+                      onChange={this.handleChange}
+                    />
+                    <Button
+                      type="submit"
+                      theme="secondary"
+                      style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em' }}
+                      onClick={this.changeAddressEditMode}
+                    >
+                      <img src={`${ APP_URL }/images/icons8-edit-24.png`} alt="Edit" />
+                    </Button>
+                  </div>
                 </div>
-              </div>
 
-              {/* Delivery Date */}
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <p style={{ color: grey2, marginBottom: '1.5em' }}>Current delivery date: Monday</p>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                  <Select name='deliveryDate'>
-                    <option value='Monday'>Monday</option>
-                    <option value='Tuesday'>Tuesday</option>
-                    <option value='Wednesday'>Wednesday</option>
-                    <option value='Thursday'>Thursday</option>
-                    <option value='Friday'>Friday</option>
-                    <option value='Saturday'>Saturday</option>
-                    <option value='Sunday'>Sunday</option>
-                  </Select>
-                  <Button
-                    type="submit"
-                    theme="secondary"
-                    style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em', color: black }}
-                  >
-                    Submit
-                  </Button>
+                {/* Delivery Date */}
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <p style={{ color: grey2, marginBottom: '1.5em' }}>Current delivery date: Monday</p>
+                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Select name='deliveryDate'>
+                      <option value='Monday'>Monday</option>
+                      <option value='Tuesday'>Tuesday</option>
+                      <option value='Wednesday'>Wednesday</option>
+                      <option value='Thursday'>Thursday</option>
+                      <option value='Friday'>Friday</option>
+                      <option value='Saturday'>Saturday</option>
+                      <option value='Sunday'>Sunday</option>
+                    </Select>
+                    <Button
+                      type="submit"
+                      theme="secondary"
+                      style={{ display: 'flex', alignItems: 'center', height: '1.9em', marginLeft: '2em', color: black }}
+                    >
+                      Submit
+                    </Button>
+                  </div>
                 </div>
-              </div>
 
-            </section>
+              </section>
 
-          </form>
-        </GridCell>
-      </Grid>
-    </>
+            </form>
+          </GridCell>
+        </Grid>
+      </>
+    )
   }
 }
 
