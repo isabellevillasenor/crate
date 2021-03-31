03b7dacb4d56717a1a86f4a076ada05a174dfe5a
tashiad <tashiadavis10@gmail.com>
2021-03-31 16:36:31 -0600

    Add starter tests

--- code/web/src/modules/user/api/state.test.js
+++ code/web/src/modules/user/api/state.test.js
@@ -1,6 +1,6 @@
-import * as state from './state.js'
+import * as state from './state.js' // this isn't working because state is an anonymous function...
 
-describe('todosReducer', () => {
+describe('user state', () => {
   it('should return state if no action is given', () => {
     const actual = dispatch(undefined, {})
 
@@ -11,41 +11,55 @@ describe('todosReducer', () => {
       details: null
     })
   })
-  //
-  // it('should return an updated state with a new todo', () => {
-  //   const action = {
-  //     type: 'ADD_TODO',
-  //     text: 'Eat a burrito',
-  //     id: 15
-  //   }
-  //
-  //   const actual = todosReducer([], action)
-  //
-  //   expect(actual).toEqual([{
-  //     text: 'Eat a burrito',
-  //     id: 15,
-  //     completed: false
-  //   }])
-  // })
-  //
-  // it('should return an updated state with a completed todo', () => {
-  //   const action = {
-  //     type: 'TOGGLE_TODO',
-  //     id: 11
-  //   }
-  //
-  //   const state = [{
-  //     text: 'eat a sandwich',
-  //     id: 11,
-  //     completed: false
-  //   }]
-  //
-  //   const actual = todosReducer(state, action)
-  //
-  //   expect(actual).toEqual([{
-  //     text: 'eat a sandwich',
-  //     id: 11,
-  //     completed: true
-  //   }])
-  // })
+
+  it('should return an updated state when a user logs in', () => {
+    // we expect this test to fail unless the seeder data is updated
+    // with the new info required for this feature
+
+    const user = {
+      name: 'The User',
+      email: 'user@crate.com',
+      role: '',
+      address: '1234 address dr, denver, co',
+      description: 'fashion lover',
+      image: 'puppy.png',
+      products: []
+    }
+
+    const action = {
+      type: 'SET_USER',
+      error: null,
+      isLoading: false,
+      isAuthenticated: true,
+      details: user
+    }
+
+    const actual = dispatch(user, action)
+
+    expect(actual).toEqual([user])
+  })
+
+  it('should return an updated state when a user updates their email', () => {
+    const user = {
+      name: 'The User',
+      email: 'updated@crate.com',
+      role: '',
+      address: '1234 address dr, denver, co',
+      description: 'fashion lover',
+      image: 'puppy.png',
+      products: []
+    }
+
+    const action = {
+      type: 'UPDATE_PROFILE',
+      error: null,
+      isLoading: false,
+      isAuthenticated: true,
+      details: user
+    }
+
+    const actual = dispatch(user, action)
+
+    expect(actual).toEqual([user])
+  })
 })
